4ed06e2cb66a23e324bee105982372ecf55372a1
BondarenkoDmitry <demianbondarenko@gmail.com>
2018-01-11 12:36:24 +0100

    SQLite MovieCursorAdapter: rating / poster?

---
+++ app/src/main/java/com/dvb/movie_db/Activities/CatalogActivity.java
@@ -0,0 +1,8 @@
+package com.dvb.movie_db.Activities;
+
+/**
+ * Created by dmitrybondarenko on 11.01.18.
+ */
+
+public class CatalogActivity {
+}
--- app/src/main/java/com/dvb/movie_db/Data/MovieCursorAdapter.java
+++ app/src/main/java/com/dvb/movie_db/Data/MovieCursorAdapter.java
@@ -2,11 +2,19 @@ package com.dvb.movie_db.Data;
 
 import android.content.Context;
 import android.database.Cursor;
+import android.media.Rating;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CursorAdapter;
+import android.widget.ImageView;
+import android.widget.RatingBar;
+import android.widget.TextView;
 
 import com.dvb.movie_db.Models.Movie;
+import com.dvb.movie_db.R;
+
+import static android.R.attr.rating;
 
 /**
  * Created by dmitrybondarenko on 10.01.18.
@@ -20,11 +28,80 @@ public class MovieCursorAdapter extends CursorAdapter {
 
     @Override
     public View newView(Context context, Cursor cursor, ViewGroup parent) {
-        return null;
+        return LayoutInflater.from(context).inflate(R.layout.sql_list_item, parent, false);
     }
 
     @Override
     public void bindView(View view, Context context, Cursor cursor) {
+        TextView aTitle = (TextView) view.findViewById(R.id.sqTitle);
+        ImageView aPosterPath = (ImageView) view.findViewById(R.id.sqPoster);
+        TextView aReleaseDate = (TextView) view.findViewById(R.id.sqReleaseDate);
+        TextView aOverview = (TextView) view.findViewById(R.id.sqOverview);
+        RatingBar aRating = (RatingBar) view.findViewById(R.id.sqRating);
+
+        int titleColumnIndex = cursor.getColumnIndex(MovieContract.MovieEntry.COLUMN_FILM_NAME);
+        int posterColumnIndex = cursor.getColumnIndex(MovieContract.MovieEntry.COLUMN_POSTER_PATH);
+        int releaseColumnIndex = cursor.getColumnIndex(MovieContract.MovieEntry.COLUMN_RELEASE_DATE);
+        int overviewColumnIndex = cursor.getColumnIndex(MovieContract.MovieEntry.COLUMN_OVERVIEW);
+        int ratingColumnIndex = cursor.getColumnIndex(MovieContract.MovieEntry.COLUMN_RATING);
+
+
+        String movieTitle = cursor.getString(titleColumnIndex);
+        String moviePoster = cursor.getString(posterColumnIndex);
+        String movieRelease = cursor.getString(releaseColumnIndex);
+        String movieOverview = cursor.getString(overviewColumnIndex);
+        String movieRating = cursor.getString(ratingColumnIndex);
+
+
+        aTitle.setText(movieTitle);
+
+        //Should I use Picasso here?
+//        aPosterPath.setImageResource();
+        aReleaseDate.setText(movieRelease);
+        aOverview.setText(movieOverview);
+
+        //I don't know how fix this one.
+//        aRating.setRating(movieRating);
 
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
